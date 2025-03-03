9c9362a5ecd22f03133c9a7d7e1fd3b6f9101fe3
Oyakodon <don@oykdn.com>
2025-03-03 18:31:13 +0900

    add: not-foundページを実装

---
+++ src/app/players/not-found.tsx
@@ -0,0 +1,21 @@
+import Alert from '@/components/Alert'
+import NavBar from '@/components/navigation/NavBar'
+
+export default function NotFound() {
+  return (
+    <main className='flex flex-col h-screen bg-slate-50 dark:bg-slate-800 '>
+      <title>Advancement Not Found</title>
+
+      <NavBar language />
+
+      <div className='lg:container lg:mx-auto p-2 flex flex-auto'>
+        <div className='flex flex-col flex-auto'>
+          <Alert
+            title='Advancement Not Found'
+            message='進捗情報の取得時にエラーが発生しました。 / An error occurred while retrieving Advancements.'
+          />
+        </div>
+      </div>
+    </main>
+  )
+}
