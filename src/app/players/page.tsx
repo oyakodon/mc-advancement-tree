03083c8792715c071d35fe57117179e49d243a21
Oyakodon <don@oykdn.com>
2025-03-03 18:32:26 +0900

    fix: ErrorFallbackをnotFound()に変更

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 import { z } from 'zod'
 
-import ErrorFallback from '@/components/modal/ErrorFallback'
 import BreadcrumbNav from '@/components/navigation/BreadcrumbNav'
 import NavBar from '@/components/navigation/NavBar'
 import PlayerProgress from '@/components/progress/PlayerProgress'
@@ -58,7 +58,7 @@ export default async function Player({ searchParams }: Props) {
   const tree = parsed.success ? await getTree(parsed.data) : null
 
   if (!parsed.success || !player || !tree) {
-    return <ErrorFallback message='進捗情報取得時にエラーが発生しました。' />
+    notFound()
   }
 
   return (
--- src/app/worlds/[id]/page.tsx
+++ src/app/worlds/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-import ErrorFallback from '@/components/modal/ErrorFallback'
 import BreadcrumbNav from '@/components/navigation/BreadcrumbNav'
 import NavBar from '@/components/navigation/NavBar'
 import PlayerRanking from '@/components/player/PlayerRanking'
@@ -46,7 +46,7 @@ export default async function World({ params }: Props) {
   const players = await getPlayers(id)
 
   if (!world || !players) {
-    return <ErrorFallback message='ワールド情報取得時にエラーが発生しました。' />
+    notFound()
   }
 
   return (
