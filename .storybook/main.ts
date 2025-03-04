9e7a284b0bfee2a7592a589d5f7400573bb85253
Oyakodon <don@oykdn.com>
2025-03-04 18:01:38 +0900

    chore: disableTelemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,22 +2,20 @@ import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
   ],
-
+  core: {
+    disableTelemetry: true,
+  },
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-
   staticDirs: ['../public'],
-
   docs: {},
-
   typescript: {
     reactDocgen: 'react-docgen-typescript',
   },
