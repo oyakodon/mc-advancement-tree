d6b36cc5531f6b878f99800dab8a32ca81cfbe86
Oyakodon <don@oykdn.com>
2025-03-06 22:53:14 +0900

    add: CF_PAGES_URLを使うよう変更

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Minecraft Advancement Tree
 
 ```
 bun install
-echo "APP_URL=http://localhost:3000/" > .env.local
+echo "CF_PAGES_URL=http://localhost:3000/" > .env.local
 bun dev
 ```
 
--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -2,7 +2,7 @@ import { ClientRequestOptions, hc } from 'hono/client'
 
 import { type AppType } from '@/app/api/[[...route]]/route'
 
-export const client = hc<AppType>(process.env.APP_URL!)
+export const client = hc<AppType>(process.env.CF_PAGES_URL!)
 
 export const options: ClientRequestOptions = {
   init: {
