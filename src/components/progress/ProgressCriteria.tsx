d7f135f692ebc25c67c654a8a000e2af2dd20227
Oyakodon <don@oykdn.com>
2025-03-13 14:56:44 +0900

    feat: 割合表示を改修

--- src/components/progress/ProgressCriteria.tsx
+++ src/components/progress/ProgressCriteria.tsx
@@ -5,26 +5,20 @@ import { Criterion } from '@/model/Advancement'
 import { Progress } from '@/model/Progress'
 import { ProgressNode } from '@/model/ProgressNode'
 
-const metricsTitle = {
-  allof: '(全て)',
-  anyof: '(1つ以上)',
-}
-
-const CriteriaHeader = ({
-  progress,
-  metrics,
-  completed,
-}: {
-  progress: Progress
-  metrics: 'allof' | 'anyof'
-  completed: boolean
-}) => {
+const CriteriaHeader = ({ progress, completed }: { progress: Progress; completed: boolean }) => {
   const { done, total } = progress
-  const percentage = done > 0 ? done / total : 0
+  const ratio = `${done || 0} / ${total || 0}`
+
+  const percentage = `(${((done / total) * 100).toFixed(0) || 0} %)`
 
   return (
-    <div className='flex justify-between items-center w-full'>
-      <span className='flex'>{'条件' + (total > 1 ? `: ${metricsTitle[metrics]}` : '')}</span>
+    <div className='flex justify-between items-center w-full h-8'>
+      <div className='flex text-sm items-center gap-2'>
+        <ChartPie className='size-4' />
+        <span className='pb-0.5 tracking-tighter'>
+          {ratio + (total > 1 ? ` ${percentage}` : '')}
+        </span>
+      </div>
 
       <span className='flex border-2 rounded-sm p-1 bg-white'>
         {total > 1 && metrics == 'allof' ? (
