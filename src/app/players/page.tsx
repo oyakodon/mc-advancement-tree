1ad11c9d3b2fa958337da62333822c72bf552220
Oyakodon <don@oykdn.com>
2025-03-13 21:02:13 +0900

    refactor: schemaから生成したtypeを使うように

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -11,6 +11,14 @@ import { client, options } from '@/lib/hono'
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
 
+const paramSchema = z.object({
+  w: z.coerce.string(),
+  p: z.coerce.string(),
+  lang: z.coerce.string().optional(),
+})
+
+type Param = z.infer<typeof paramSchema>
+
 const getPlayer = async ({ p: id }: { p: string }) => {
   const res = await client.api.v1.players[':id'].$get(
     {
@@ -21,7 +29,7 @@ const getPlayer = async ({ p: id }: { p: string }) => {
   return res.ok ? await res.json() : null
 }
 
-const getTree = async ({ w, p, lang }: { w: string; p: string; lang?: string }) => {
+const getTree = async ({ w, p, lang }: Param) => {
   const res = await client.api.v1.tree.$get(
     {
       query: { w, p, lang },
@@ -31,18 +39,12 @@ const getTree = async ({ w, p, lang }: { w: string; p: string; lang?: string }) 
   return res.ok ? await res.json() : null
 }
 
-const schema = z.object({
-  w: z.coerce.string(),
-  p: z.coerce.string(),
-  lang: z.coerce.string().optional(),
-})
-
 type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
-  const parsed = schema.safeParse(await searchParams)
+  const parsed = paramSchema.safeParse(await searchParams)
   const player = parsed.success ? await getPlayer(parsed.data) : null
 
   return {
@@ -56,7 +58,7 @@ const navItems = (worldId: string, name: string) => [
 ]
 
 export default async function Player({ searchParams }: Props) {
-  const parsed = schema.safeParse(await searchParams)
+  const parsed = paramSchema.safeParse(await searchParams)
   const player = parsed.success ? await getPlayer(parsed.data) : null
   const tree = parsed.success ? await getTree(parsed.data) : null
 
